be43b4829e786db0feab316b227bd13701a267bd
KosarevPV <pavel999kosarev@gmail.com>
2023-03-18 21:26:51 +0300

    add main part of project

---
+++ blog_flask/__init__.py
@@ -0,0 +1,36 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from blog_flask.config import Config
+from flask_bcrypt import Bcrypt
+from flask_mail import Mail
+
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+bcrypt = Bcrypt()
+mail = Mail()
+
+
+def create_app():
+    print(__name__)
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    login_manager.init_app(app)
+    bcrypt.init_app(app)
+    mail.init_app(app)
+
+    from blog_flask.main.routes import main
+    from blog_flask.users.routes import users
+    from blog_flask.posts.routes import posts
+    from blog_flask.errors.handlers import errors
+    app.register_blueprint(main)
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(errors)
+    app.config.from_object(Config)
+
+
+    return app
---
+++ blog_flask/config.py
@@ -0,0 +1,9 @@
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
+
+    MAIL_SERVER = 'smtp.mail.ru'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = "pasha5kosarev@mail.ru"
+    MAIL_PASSWORD = "tHqpuxJFBu4NDavvBd4d"
